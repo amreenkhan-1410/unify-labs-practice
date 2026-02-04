a54a08ae77c68661ca7c220b0e9d05d924fdedc9
amreenkhan-1410 <patthan7879@gmail.com>
2026-02-04 20:53:05 +0530

    removed unorganized files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.getElementById("theme-toggle");
-
-toggle.addEventListener("click", () => {
-  const current = document.documentElement.getAttribute("data-theme");
-  document.documentElement.setAttribute(
-    "data-theme",
-    current === "dark" ? "light" : "dark"
-  );
-});

87cfd59ce129109956a902b1b49e0799e44dd5e8
P. AMREEN KHAN <patthan7879@gmail.com>
2026-02-09 22:40:48 +0530

    🚀 Day 13 - index.html - 10:40:44 PM

---
+++ Day-13/script.js
@@ -0,0 +1,27 @@
+function runProgram() {
+
+    // Variable declaration
+    let num1 = 15;
+    let num2 = 4;
+
+    const userName = "Amreen";
+
+    // Calculations
+    let sum = num1 + num2;
+    let product = num1 * num2;
+    let remainder = num1 % num2;
+
+    // Welcome message
+    let message = "Welcome, " + userName + "!";
+
+    // Console output
+    console.log(message);
+
+    console.log("Number 1:", num1, "| Type:", typeof num1);
+    console.log("Number 2:", num2, "| Type:", typeof num2);
+
+    console.log("Sum:", sum);
+    console.log("Product:", product);
+    console.log("Remainder:", remainder);
+}
+
